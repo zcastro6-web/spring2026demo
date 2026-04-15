025f50b8c848274dcdfa7fba7bc0e47d24863e96
zcastro6-web <zcastro6@horizon.csueastbay.edu>
2026-04-15 01:40:26 -0700

    Update mySketch.js.js

--- mySketch.js.js
+++ mySketch.js.js
@@ -963,6 +963,6 @@ function distToSegment(px, py, x1, y1, x2, y2) {
 }
 
 function setup() {
-  let canvas = createCanvas(620, 360);
+  let canvas = createCanvas(720, 360);
   canvas.parent("game-container");
 }
